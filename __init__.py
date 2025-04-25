1fc617e043f9d9a4c275d06d04ab50c0ba61d9f5
D15k <165778438+D15k@users.noreply.github.com>
2025-04-25 23:25:54 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,30 @@
+'''
+PlasmaSim: A small library for plasma simulation in 1D + 1D phase space.
+
+Provides:
+  - Simulator: the main driver class
+  - Params: a container for simulation parameters
+  - save_config / load_config: io helpers for your “data” dict
+'''
+
+__version__ = '0.1.0'
+
+# Package API
+
+from .initialization import Parameters, Species
+from .simulation import Simulation
+from .solvePoisson import Poisson
+from .solveVlasov import NuFi#, CMM, predictor_corrector
+#from .io    import save_config, load_config
+#from .plot  import plot_results
+
+import jax
+jax.config.update("jax_enable_x64", True)
+
+__all__ = [
+  'Simulation',
+  'Parameters',
+  'Species',
+  'Poisson',
+  'NuFi'
+]
---
+++ plotting.py
@@ -0,0 +1,1 @@
+# Plotting fs and Efield
